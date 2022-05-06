827d9525bfe7541b225968ceb60a0651ae1f286e
Andrew Myers <andru@cs.cornell.edu>
2022-05-05 21:20:12 -0400

    turn off popup on failure

--- abstracts.js
+++ abstracts.js
@@ -6,9 +6,10 @@ function setup_abstract(name) {
     }
     const a = EZDom.span({className: 'popup', id: name + "_abstract"})
     t.parentNode.insertBefore(a, t.nextSibling)
-    fetch_content(a.id, 'abstracts/' + name + '.html')
-    t.onmouseenter = (e) => a.style.display = 'block';
-    t.onmouseleave = (e) => a.style.display = 'none';
+    fetch_content(a.id, 'abstracts/' + name + '.html', () => {
+        t.onmouseenter = (e) => a.style.display = 'block';
+        t.onmouseleave = (e) => a.style.display = 'none';
+    })
 }
 function appear(id) {
     document.getElementById(id).style.display = 'block'
